0db7ba266731f81bb64eb2b1314ec77b602f2dde
Jonathan <jonathanchristian1221@gmail.com>
2024-09-20 11:11:41 +0700

    update index.js

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ const server = http.createServer( (req,res) => {
     // res.write('Selamat Pagi');
 
     res.writeHead(200, { 'Content-Type' : 'application/json'});
+
+    if(req.url === '/dosen'){
+        message = 'List data dosen';
+        data = ['Ahmad', 'Hafiz', 'Widi'];
+    } else if(req.url === '/mahasiswa') {
+        message = 'List data mahasiswa';
+        data = ['Kenny', 'Rizky', 'Akbar'];
+    } else {
+        message = 'Tidak ditemukan';
+        data = [];
+    }
     res.write(JSON.stringify({
-        'message' : 'selamat pagi',
+        'message' : message,
+        'data' : data,
         'status' : 'success'
     }));
-    
     res.end();
 } );
 
