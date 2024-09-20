c38b88d982de189270446d66f3562deabf5cd333
Jonathan <jonathanchristian1221@gmail.com>
2024-09-20 11:03:25 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const http = require('http');
 
 // buat server
 const server = http.createServer( (req,res) => {
-    res.writeHead(200, { 'Content-Type' : 'text/html'});
-    res.write('Selamat Pagi');
+    // res.writeHead(200, { 'Content-Type' : 'text/html'});
+    // res.write('Selamat Pagi');
+
+    res.writeHead(200, { 'Content-Type' : 'application/json'});
+    res.write(JSON.stringify({
+        'message' : 'selamat pagi',
+        'status' : 'success'
+    }));
+    
     res.end();
 } );
 
